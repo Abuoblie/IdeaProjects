c47262d601ccd166e3773354203c92b1cc0c0c6e
Abuoblie <Abu.oblie@gmail.com>
2022-10-29 10:34:08 +0200

    update 1

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,23 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        System.out.println("Bievenu au puissance 4 consol!");
+        System.out.println("################### Bievenu au puissance 4 consol! #######################");
         P4Game game = new P4Game();
+        int slot=1, mode;
+        System.out.println("######################### Puissance 4 ####################################");
+        System.out.println("################### Bien venu au puissance ###############################");
+        System.out.println("######################## Mode de jeur ####################################");
+        System.out.println("################# 1) jouer contre le consol ##############################");
+        System.out.println("############ un autre chiffre pour jouer contre un ami ###################");
+        mode = P4Game.parseInput(kb);
+        boolean result = false;
 
-
-        for (int u=0; u<15; u++){
-            String x=(u%2 == 0)?"X":"Y";
-            game.play(kb,x);
+        while (!result && slot < 42 ){
+            String x=(slot%2 == 0)?"X":"Y";
+            result = game.play(kb,x,mode,slot);
+            slot++;
         }
-
 
     }
 }
+
--- src/puissance4/P4Game.java
+++ src/puissance4/P4Game.java
@@ -1,5 +1,6 @@
 package puissance4;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class P4Game {
@@ -11,17 +12,17 @@ public class P4Game {
     private String[][] board = new String[6][7];
 
     public P4Game() {
-        System.out.println("welcome to my puissance 4");
 
         for(int i = 0; i < 6; ++i) {
+            System.out.print("########################");
             for(int j = 0; j < 7; ++j) {
-                this.board[i][j] = "o";
-                System.out.print("|o|");
+                this.board[i][j] = "0";
+                System.out.print("|0|");
             }
 
-            System.out.println();
+            System.out.println("#############################");
         }
-
+        System.out.println("##########################################################################");
     }
     public static int parseInput(Scanner kb)
     {
@@ -38,25 +39,29 @@ public class P4Game {
 
     public void showBoad() {
         for(String[] line: board){
+            System.out.print("########################");
             for (String cel:line) {
 
                 System.out.print("|"+cel+"|");
             }
-            System.out.println();
+            System.out.println("#############################");
         }
+        System.out.println("##########################################################################");
     }
+
     public boolean  horizontalWinningSlot(int col,String input){
         int i =this.colHight[col];
 
         //start counting  from three cells back
         int k=col-3<0? 0 : col-3;
-        int counter = 0;
-
+        int lim = col+3>6? 6 : col+3;
         //check for four consecutive matches
-        for (int stCol = k;stCol<=col; stCol++ ){
-            int j = stCol;
-            while (this.board[i][j].equals(input) && counter<4 && j <6){
+        for (int stCol = k; stCol<=col; stCol++ ){
+            int j = stCol,counter = 0;
+
+            while (this.board[i][j].equals(input) && counter<4 && j<lim){
                 counter++;
+
                 j++;
             }
             if (counter == 4){
@@ -67,103 +72,169 @@ public class P4Game {
     }
     public boolean verticalWinningSlot(int col, String input){
         int i =this.colHight[col];
+        int lim = i+3>5? 5 : i+3;
         int counter = 0;
         while (this.board[i][col].equals(input) && counter<4){
             counter++;
-            if (i<5){i++;}
+
+            if (i<lim)i++;
         }
         if (counter == 4){
+
             return true;
         }
         return false;
     }
     public boolean  rightDiagonalWinningSlot(int col,String input){
-        //obtain the row index from colHight
-        int a =this.colHight[col];
-        //start counting  from three cells back
-        int n = a+3>5? 5 : a+3;
-        int k=col-3<0? 0 : col-3;
-        int counter = 0;
-
-        for (int b = n,c=k; a>=a && c<=col ; c++, b--){
-            int i = b, j =c;
-
-            while (this.board[i][j].equals(input)  && counter<4){
+        if (col == 6){
+            int startRowIndex =colHight[col];
+            int i = startRowIndex, j =col, counter=0;
+            while (board[i][j].equals(input) && counter<4){
                 counter++;
-                // System.out.println(" i = "+i+"j = "+j);
-                i--;
-                if (j<6){j++;}
-
+                if(i<5)i++;
+                if(j>0)j--;
             }
-            if (counter == 4){
+            if (counter == 4) {
                 return true;
             }
+        } else if (col>2 && colHight[col]<3) {
+            int row =colHight[col];
+            int colStart =col+row>6? 6 : col+row;
+            int startRowIndex = row+col-6<0? 0 : row+col-6;
 
+            for (int i = startRowIndex, j = colStart; i <= row && j >=col; i++, j--){
+                int counter = 0;
+
+                while (board[i][j].equals(input) && counter<4){
+                    counter++;
+                    if(i<5)j++;
+                    if(j>0)j--;
+
+                }
+                if (counter == 4) {
+                    return true;
+                }
+
+            }
         }
         return false;
     }
     public boolean  leftDiagonalWinningSlot(int col, String input){
-        //obtain the row index from colHight
-        int a =this.colHight[col];
-        //start counting  from three cells back
-        int n = a-3<0? 0 : a-3;
-        int k=col-3<0? 0 : col-3;
-        System.out.println(" n = "+n+"k = "+k);
-        int counter = 0;
-        for (int b = n, c=k; c<=col && b<=a; c++, b++){
-            int i = b, j =c;
-
-            while (this.board[i][j].equals(input)  && counter<4){
-
+        if (col == 0){
+            int startRowIndex =colHight[col];
+            int i = startRowIndex, j =col, counter=0;
+            while (board[i][j].equals(input) && counter<4){
                 counter++;
-                if (i<5){i++;}
-                if (j<6){j++;}
+                if(i<5)i++;
+                if(j<6)j++;
             }
-            if (counter == 4){
+            if (counter == 4) {
                 return true;
             }
+        } else if (col<4 && colHight[col]<3) {
+            int row =colHight[col];
+            int colStart =col-row<0? 0 : col-row;
+            int startRowIndex = col-row<0? (col-row)+row: 0;
+            for (int i = startRowIndex, j = colStart; i<= row && j <=col; i++, j++){
+                int counter = 0;
 
+                while (board[i][j].equals(input) && counter<4){
+                    counter++;
+                    if(i<5)i++;
+                    if(j<6)j++;
+                }
+                if (counter == 4) {
+                    return true;
+                }
+
+            }
         }
+
         return false;
     }
-    public boolean play(Scanner kb,String x){
-        System.out.println("merci d'encoder le numéro(0 à 6) de la colonne où vous souhaitez déposer vos jeton");
-        int col = this.parseInput(kb);
-        this.board[this.colHight[col]][col]=x;
-        this.totalslots+=1;
+    public boolean play(Scanner kb,String x, int mode , int slot) {
 
-        if (this.totalslots>=7){
+        int col = this.col(kb,mode,slot);
+        int row = this.colHight[col];
+        this.board[row][col] = x;
+        this.totalslots += 1;
+        boolean playerXv = false,playerYv=false,playerXh=false,playerYh=false,playerXDl=false,playerXDR=false,playerYDl=false,playerYDR=false;
 
-            //check for horizontal  and vertical alignment
-            boolean playerXh = this.horizontalWinningSlot(col,"X");
-            boolean playerXv = this.verticalWinningSlot(col,"X");
+        if (this.totalslots >= 7 && row <= 2) {
+            //check for vertical alignment
+            playerXv = this.verticalWinningSlot(col, "X");
+            playerYv = this.verticalWinningSlot(col, "Y");
+        }
+        if (this.totalslots >= 7 && !(playerXv||playerYv)) {
+            //check for horizontal
+            playerXh = this.horizontalWinningSlot(col, "X");
+            playerYh = this.horizontalWinningSlot(col, "Y");
+        }
+        if (this.totalslots >= 10 && !(playerXh||playerYh) ){
+            //check for diagonal alignment
+            playerXDl = this.leftDiagonalWinningSlot(col, "X");
+            playerXDR = this.rightDiagonalWinningSlot(col, "X");
 
+            playerYDl = this.leftDiagonalWinningSlot(col, "Y");
+            playerYDR = this.rightDiagonalWinningSlot(col, "Y");
+        }
+        this.showBoad();
 
-            boolean playerYh = this.horizontalWinningSlot(col,"Y");
-            boolean playerYv = this.verticalWinningSlot(col,"Y");
-
-
-                //check for diagonal alignment
-            boolean playerXDl = this.leftDiagonalWinningSlot(col,"X");
-            boolean playerXDR = this.rightDiagonalWinningSlot(col,"X");
-
-            boolean playerYDl = this.leftDiagonalWinningSlot(col,"Y");
-            boolean playerYDR = this.rightDiagonalWinningSlot(col,"Y");
-
-            if (playerXh || playerXv || playerXDl ||playerXDR ){
+        if (playerXh || playerXv || playerXDl ||playerXDR ){
                 System.out.println("playerX a gagner");
-                return  true;
-            }
-            else if (playerYh ||playerYv|| playerYDl ||playerYDR ){
+                return true;
+
+        }
+        else if (playerYh ||playerYv|| playerYDl ||playerYDR ){
                 System.out.println("playerY a gagner");
                 return true;
-            }
         }
+
 
         this.colHight[col]-=1;
         this.entrePerCol[col]+=1;
-        this.showBoad();
+
         return false;
+    }
+    public int col(Scanner kb,int mode, int slot){
+        int col=-1;
+        if(mode == 1){
+            Random random = new Random();
+            if(slot%2==0){
+                System.out.println("merci d'encoder le numéro(0 à 6) de la colonne où vous souhaitez déposer vos jeton ");
+                do {
+                    col = this.parseInput(kb);
+                    if (this.colHight[col]<0 ){
+                        System.out.println("la colonne choisie est pleine veuillez en choisir une autre");
+                    } else if (col<0 || col>6) {
+                        System.out.println("numero invalid: merci d'encoder un numéro de 0 à 6");
+                    }
+                }while (this.colHight[col]<0 || (col<0 || col>6));
+
+            }else if (slot%2==1){
+
+                col = random.nextInt(7);
+                while (this.colHight[col]<0){
+                    col = random.nextInt(7);
+                }
+            }
+
+        }
+        else {
+            System.out.println("merci d'encoder le numéro(0 à 6) de la colonne où vous souhaitez déposer vos jeton ");
+            do {
+                col = this.parseInput(kb);
+                if (this.colHight[col]<0 ){
+                    System.out.println("la colonne choisie est pleine veuillez en choisir une autre");
+                } else if (col<0 || col>6) {
+                    System.out.println("numero invalid: merci d'encoder un numéro de 0 à 6");
+                }
+            }while (this.colHight[col]<0 || (col<0 || col>6));
+
+        }
+
+
+        return  col;
     }
 
 }
